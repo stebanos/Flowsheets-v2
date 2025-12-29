d0bf57dee1e79e54791a95fb33da970812f19680
stebanos <stefan.gabriels@gmail.com>
2025-12-29 08:12:50 +0100

    Wrap block name in header div to accommodate menu button

--- src/features/block-editor/components/Block.vue
+++ src/features/block-editor/components/Block.vue
@@ -51,10 +51,11 @@ const isHighlighted = computed(() => hovered.value === props.block.name);
 <template>
     <div class="absolute !box-content select-none border bg-white shadow-md text-[.875rem] leading-[1rem] flex flex-col"
          :style="blockPositionStyle" :class="isHighlighted ? 'border-black' : 'border-gray-300'">
-        <block-name v-model:name="block.name"
-            class="px-2 block-name min-h-6 h-6 flex items-center justify-center w-full cursor-move border-b border-gray-300"
-            :class="isHighlighted ? 'bg-yellow-200 text-black' : 'bg-black text-white'"
-            @mousedown="startDrag(block, $event)" />
+        <div class="px-2 border-b border-gray-300" :class="isHighlighted ? 'bg-yellow-200 text-black' : 'bg-black text-white'">
+            <block-name v-model:name="block.name"
+                class="block-name min-h-6 h-6 flex items-center justify-center w-full cursor-move"
+                @mousedown="startDrag(block, $event)" />
+        </div>
         <div class="block-code flex-1 min-h-0 w-full">
             <code-editor class="block-code-editor h-full w-full overflow-auto" v-model:code="block.code" />
         </div>
