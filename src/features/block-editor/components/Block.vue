01ceb89aa74bcd8509f596f7dc9ac189fe958759
stebanos <stefan.gabriels@gmail.com>
2026-03-08 06:19:21 +0100

    Remove !box-content override from block container

--- src/features/block-editor/components/Block.vue
+++ src/features/block-editor/components/Block.vue
@@ -50,7 +50,7 @@ const isHighlighted = computed(() => hovered.value === props.block.name);
 </script>
 
 <template>
-    <div class="group absolute !box-content select-none border bg-white shadow-md text-[.875rem] leading-[1rem] flex flex-col"
+    <div class="group absolute select-none border bg-white shadow-md text-[.875rem] leading-[1rem] flex flex-col"
          :style="blockPositionStyle" :class="[isHighlighted ? 'border-black' : 'border-gray-300', {'menu-visible': isMenuOpen}]">
         <div class="block-header relative px-2 has-[input]:px-0.25 border-b border-gray-300" :class="isHighlighted ? 'bg-yellow-200 text-black' : 'bg-black text-white'">
             <block-menu :block class="block-menu absolute not-group-hover:invisible group-has-[input]:invisible group-[.menu-visible]:visible" @menu-toggle="isMenuOpen = $event" />
